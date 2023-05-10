efb23918a427d6a2781234ace285eb981e292d40
Taekyun Kim <taekkim@student.42seoul.kr>
2023-05-11 03:37:52 +0900

    Initial Commit

---
+++ CallClone/CallCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallCloneApp.swift
+//  CallClone
+//
+//  Created by taekkim on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct CallCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
